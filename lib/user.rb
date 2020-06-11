7b9b2aba739df4005247ecdf3854fe321b7c7a9b
travis Lawson <travislawclimb@gmail.com>
2020-06-11 18:04:21 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,18 +2,18 @@ class User
   
   
   
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end 
+  # def initialize(first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end 
   
-  def first_name
-    @first_name
-  end 
+  # def first_name
+  #   @first_name
+  # end 
   
-  def last_name
-    @last_name
-  end   
+  # def last_name
+  #   @last_name
+  # end   
   
   
 end   
