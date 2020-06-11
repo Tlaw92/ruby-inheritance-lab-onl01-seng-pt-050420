90cc794b6700a63e86f9105bbf3caf931f65491e
travis Lawson <travislawclimb@gmail.com>
2020-06-11 18:02:55 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,8 +2,8 @@ class User
   
   # attr_accessor :first_name :last_name
   
-  def first_name(first_name)
-    @first_name= first_name
+  def initialize(first_name)
+    @first_name = first_name
   end 
   
   def first_name
